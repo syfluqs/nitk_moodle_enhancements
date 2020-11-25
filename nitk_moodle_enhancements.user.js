db97ce7fd36647a119e6aa13eff440a19f2164e4
syfluqs <syfluqs@gmail.com>
2020-11-25 20:25:06 +0530

    fix version bump

--- nitk_moodle_enhancements.user.js
+++ nitk_moodle_enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nitk moodle enhancements
 // @namespace    https://lectures.iris.nitk.ac.in
-// @version      0.10
+// @version      0.95
 // @description  some bug fixes and enhancements for nitk moodle platform
 // @author       roy
 // @match        https://lectures.iris.nitk.ac.in/playback/*
